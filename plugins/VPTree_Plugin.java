2d3a2ea12e4301595b75a6b5f1ee25ddf0d5f668
joshuanostas <joshuanostas49@gmail.com>
2021-03-11 18:04:02 -0400

    Add VP_TREE plugin

--- plugins/VPTree_Plugin.java
+++ plugins/VPTree_Plugin.java
@@ -7,6 +7,7 @@ import static ij.plugin.filter.PlugInFilter.DOES_ALL;
 import static ij.plugin.filter.PlugInFilter.DOES_RGB;
 import static ij.plugin.filter.PlugInFilter.DONE;
 import ij.process.ImageProcessor;
+import util.VpTree;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -21,16 +22,19 @@ import ij.process.ImageProcessor;
 public class VPTree_Plugin implements PlugInFilter {
     
     private ImagePlus imp;
+    private ImageProcessor ip;
     private int patch_size, num_patches, nbTrees;
     private int nbPatches;
-    private float[] idxPatches;
+    private int[] idxPatches;
     private int width, height, nChannels;
+    int[] image;
+    private VpTree trees[];
 
     @Override
-    public int setup(String string, ImagePlus ip) {
+    public int setup(String string, ImagePlus imp) {
         this.imp=imp;
         GenericDialog gd = new GenericDialog("VP Trees");
-        gd.addNumericField("Parch size:", 8);
+        gd.addNumericField("Patch size:", 8);
         gd.addNumericField("Number of Patches:", 16);
         gd.showDialog();
         if (gd.wasCanceled())
@@ -38,32 +42,56 @@ public class VPTree_Plugin implements PlugInFilter {
         patch_size=(int)gd.getNextNumber();
         num_patches=(int)gd.getNextNumber();
         nbTrees=4;
-        return IJ.setupDialog(imp,DOES_ALL-DOES_RGB);
+        return IJ.setupDialog(imp,DOES_ALL);
     }
 
     @Override
     public void run(ImageProcessor ip) {
-        setValues();
+        setValues(ip);
+        createTrees();
     }
     
-    public void setValues() {
-        width=imp.getWidth();
-        height=imp.getHeight();
-        nChannels=imp.getNChannels();
-        nbPatches=(width - patch_size + 1) * (height - patch_size + 1);
-        getAllPackages();
+    public void setValues(ImageProcessor ip) {
+        this.ip = ip;
+        image = (int[]) ip.getPixels();
+        width = ip.getWidth();
+        height = ip.getHeight();
+        nChannels = ip.getNChannels();
+        trees = new VpTree[nbTrees];
+        nbPatches = (width - patch_size + 1) * (height - patch_size + 1);
+        idxPatches = new int[nbPatches];
+        getAllPatches();
     }
     
-    private void getAllPackages() {
+    private void getAllPatches() {
         for (int x = 0, k = 0; x < width - patch_size + 1; x++) {
             for (int y = 0; y < height - patch_size + 1; y++, k++) {
-                idxPatches[k] = nChannels * (x + y * width);
+                idxPatches[k] = (x + y * width);
             }
         }
     }
     
-    private void splitTree() {
-        
+    private void createTrees() {
+        for (int i = 0; i < nbTrees; i++) {
+            trees[i] = new VpTree(num_patches, idxPatches, patch_size, ip);
+        }
+        for (int i = 0; i < nbTrees; i++) {
+            System.out.println("Tree " + i);
+            trees[i].printTree(trees[i].root);
+        }
     }
+    
+    /*        r = new int[size];
+        g = new int[size];
+        b = new int[size]; 
+        int[] pixels=(int[])ip.getPixels();
+        for (i= 0;i<size;i++)
+        {
+            int c=pixels[i];
+            r[i]=(c&0xff0000)>>16;
+            g[i]=(c&0x00ff00)>>8;
+            b[i]=c&0x0000ff;
+        }
+    */
     
 }
--- plugins/util/VpTree.java
+++ plugins/util/VpTree.java
@@ -5,7 +5,9 @@
  */
 package util;
 
+import ij.process.ImageProcessor;
 import static java.lang.Math.max;
+import java.util.Random;
 
 /**
  *
@@ -13,18 +15,33 @@ import static java.lang.Math.max;
  */
 public class VpTree {
     
-    Node root;
+    public Node root;
     int kNN;
     int leaf_max_size;
     int nbSelectionVP;
     int subSampling;
+    int patchSize;
     int[] idxPatches;
+    int[] image;
+    private int width, height, nChannels;
+    private ImageProcessor ip;
     
-    public VpTree(int nSimilarPatches, int[] nbPatches) {
+    public VpTree(int nSimilarPatches, int[] nbPatches, int patch_size, ImageProcessor ip) {
         this.kNN = nSimilarPatches;
         this.leaf_max_size = max(2 * this.kNN + 1, 5);
         this.idxPatches = nbPatches;
-        this.root = this.splitTree(0, this.idxPatches.length);
+        this.patchSize = patch_size; 
+        this.ip = ip;
+        this.setValues();
+        this.root = this.splitTree(0, this.idxPatches.length - 1);
+    }
+    
+    private void setValues() {
+        image = (int[]) ip.getPixels();
+        width = ip.getWidth();
+        height = ip.getHeight();
+        nChannels = ip.getNChannels();
+        nbSelectionVP = 1;
     }
     
     private Node splitTree(int left, int right) {
@@ -39,7 +56,7 @@ public class VpTree {
             tempNode.splitDistance = 0;
 	} else {
             float[] distances = new float[right - left + 1];
-            float vantage = this.computeVantagePoint(left, right);
+            int vantage = this.computeVantagePoint(left, right);
             float maxDist = 0;
             
             for(int i = 0, j = left; j <= right; i++, j++)
@@ -86,15 +103,140 @@ public class VpTree {
         }
     }
     
-    private float computeVantagePoint(int left, int right) {
-        return 0;
+    private int computeVantagePoint(int left, int right) {
+        this.shuffleArray(idxPatches, left, right + 1);
+        int[] elementToConsider = new int[nbSelectionVP];
+        
+        for (int i = 0; i < nbSelectionVP; i++) {
+            elementToConsider[i] = idxPatches[left + i];
+        }
+        
+        float maxVar = 0;
+	int interestingPoint = 0;
+	if(nbSelectionVP == 1)
+	{
+            // Construct a VP tree for a forest, in this case no other computation need to be done. We choose a random VP
+            return elementToConsider[0];
+	} else {
+            // Otherwise select the best candidates from a subsample of possible vantage points
+            elementToConsider = new int[nbSelectionVP];
+            int[] subSample = new int[(Math.min(right-left+1, subSampling))];
+            float[] distancesToSubs = new float[(Math.min(right - left + 1, subSampling))];
+            
+            this.shuffleArray(idxPatches, left, right + 1);
+            
+            for (int i = 0; i < subSample.length; i++) {
+                subSample[i] = idxPatches[left + i];
+            }
+            
+            for(int i = 0; i < nbSelectionVP; ++i) {
+                for(int j = 0; j < subSample.length; ++j) {
+                    distancesToSubs[j] = this.distance(elementToConsider[i], subSample[j]); 
+                }
+                
+                float medianTemp = quickselect(left, left + distancesToSubs.length - 1, distancesToSubs);
+                
+                float var = 0;
+                for (int j = 0; j < distancesToSubs.length; j++) {
+                    var += (distancesToSubs[j] - medianTemp) * (distancesToSubs[j] - medianTemp);
+                }
+                if(var > maxVar)
+                {
+                    maxVar = var;
+                    interestingPoint = i;
+                }
+            }
+        }
+        return elementToConsider[interestingPoint];
     }
     
-    private float distance(float patch1, int patch2) {
-        return 0;
+    private float distance(int patch1, int patch2) {
+        float dist = 0.f, dif;
+        for (int hy = 0; hy < this.patchSize; hy++) {
+            for (int hx = 0; hx < this.patchSize; hx++) {
+                float p1 = this.image[patch1 + hx + hy * width];
+                float p2 = this.image[patch2 + hx + hy * width];
+                dif = p1 - p2;
+                dist += (p1 - p2) * dif;
+                
+            }
+        }
+        return (float) Math.sqrt(dist / (this.patchSize * this.patchSize)) / 255.f;
     }
 
     private int partition(int left, int right, int pivot, float[] distances, int n, int l) {
-        return 0;
+        float valuePivot = distances[pivot - l];
+
+	int temp = idxPatches[pivot];
+	idxPatches[pivot] = idxPatches[right];
+	idxPatches[right] = temp;
+	float tempF = distances[pivot - l];
+	distances[pivot - l] = distances[right - l];
+	distances[right - l] = tempF;
+
+	int store = left;
+	int allEqual = 0;
+        
+        for (int i = left; i <= right; i++) {
+            float candidate = distances[i - l];
+            
+            if(Math.abs(candidate - valuePivot) < 0.000001) {
+                allEqual++;
+            }
+            
+            if(candidate < valuePivot) {
+		temp = idxPatches[store];
+		idxPatches[store] = idxPatches[i];
+		idxPatches[i] = temp;
+		tempF = distances[store - l];
+		distances[store - l] = distances[i - l];
+		distances[i - l] = tempF;
+		++store;
+            }
+        }
+
+        temp = idxPatches[store];
+	idxPatches[store] = idxPatches[right];
+	idxPatches[right] = temp;
+	tempF = distances[store - l];
+	distances[store - l] = distances[right - l];
+	distances[right - l] = tempF;
+
+	if(allEqual >= (right - left) / 2) {
+            return n;
+        }
+
+	// return the index where the pivot is
+	return store;
+    }
+    
+    private void shuffleArray(int[] a, int primero, int ultimo) {
+        int n = a.length;
+        Random random = new Random();
+        random.nextInt();
+        for (int i = primero; i < ultimo; i++) {
+            if (n>i) {
+                int change = i + random.nextInt(n - i);
+                swap(a, i, change);
+            }
+            
+        }
+    }
+
+    private void swap(int[] a, int i, int change) {
+        int helper = a[i];
+        a[i] = a[change];
+        a[change] = helper;
+    }
+    
+    public void printTree(Node raiz) {
+        System.out.println("Vantage" + raiz.vantagePoint);
+        System.out.println("Distance" + raiz.splitDistance);
+        if (raiz.leftChild != null) {
+            printTree(raiz.leftChild);
+        }
+        if (raiz.rightChild != null) {
+            printTree(raiz.rightChild);
+        }
     }
 }
